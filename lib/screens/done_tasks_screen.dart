2f6b88744d652d5b40ccd0c4fa6257700171bd27
otouto <otoutoyeah@gmail.com>
2020-05-05 00:30:27 +0900

    終了タスクのindicatorの作成

--- lib/screens/done_tasks_screen.dart
+++ lib/screens/done_tasks_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:todoapp/models/todo_model.dart';
 import 'package:provider/provider.dart';
 import 'package:todoapp/widget/todo_list.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 class DoneTasksScreen extends StatefulWidget {
   @override
@@ -15,13 +16,37 @@ class _DoneTasksScreenState extends State<DoneTasksScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final todoModel = Provider.of<TodoModel>(context);
+    double percentTask = todoModel.completedTodoList.length /
+        (todoModel.completedTodoList.length +
+            todoModel.incompletedTodoList.length);
+    if (todoModel.incompletedTodoList.length == 0 &&
+        todoModel.completedTodoList.length == 0) {
+      percentTask = 0;
+    }
+    int taskRatio = (percentTask * 100).round();
     return Scaffold(
-      //backgroundColor: Color.fromARGB(255, 250, 243, 240),
       backgroundColor: Colors.cyan,
-
       body: SafeArea(
         child: Column(
           children: <Widget>[
+            CircularPercentIndicator(
+              radius: 200.0,
+              lineWidth: 26.0,
+              animation: true,
+              percent: percentTask,
+              center: Text(
+                "$taskRatio%",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+              ),
+//              footer:  Text(
+//                "Sales this week",
+//                style:
+//                     TextStyle(fontWeight: FontWeight.bold, fontSize: 17.0),
+//              ),
+              circularStrokeCap: CircularStrokeCap.round,
+              progressColor: Colors.purple,
+            ),
             Text(
               'Done Task',
               style: TextStyle(
@@ -31,7 +56,7 @@ class _DoneTasksScreenState extends State<DoneTasksScreen> {
               ),
             ),
             Text(
-              '${Provider.of<TodoModel>(context).completedTodoList.length} tasks',
+              '${todoModel.completedTodoList.length} tasks',
               style: TextStyle(
                 color: Colors.white,
                 fontSize: 30.0,
@@ -48,9 +73,10 @@ class _DoneTasksScreenState extends State<DoneTasksScreen> {
                   borderRadius: BorderRadius.circular(10.0),
                 ),
                 //child:
+                //Todo #1 DBを引数にするか、TodoList側でDBを定義する(consumerを使う？)
                 child: TodoList(
-                  todoList: Provider.of<TodoModel>(context).completedTodoList,
-                  todoModel: Provider.of<TodoModel>(context),
+                  todoList: todoModel.completedTodoList,
+                  todoModel: todoModel,
                 ),
               ),
             ),
